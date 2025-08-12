8ebb788edab0789e88cce8c0a399ea63cce5ee61
Steve <cautran2911@gmail.com>
2025-08-12 23:34:24 +0700

    dadsa

--- api_request/insert_records.py
+++ api_request/insert_records.py
@@ -1,8 +1,18 @@
+import psycopg2
 from api_request import mock_fetch_data
 
 def connect_to_db():
     print('Connecting to the PostgreSQL database..')
     try:
-        psy
-
-    except:
+        conn = psycopg2.connect(
+        host = "localhost",
+        port = 5000,
+        dbname="db",
+        user = "db_user",
+        password = "db_password"
+        )
+        return conn
+    except psycopg2.Error as e:
+        print(f"Database connection failed:{e}")
+        raise
+connect_to_db()

92a4693cf4e696171575d9ee5befb577d54c3611
Steve Tran <ngoccau2911@gmail.com>
2025-09-11 10:46:19 +0000

    2nd update

--- api_request/airflow/dags/orchestrator.py
+++ api_request/airflow/dags/orchestrator.py
@@ -3,27 +3,25 @@ from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-sys.path.append('/opt/airflow/api')
+sys.path.append('/opt/airflow/api_request')
 
 def safe_main_callable():    
-    from api.insert_records import main
+    from api_request.insert_records import main
     return main()
 
 dèfault_args = {
     'description': 'DAG to orchestrate weather data fetching and insertion',
-    'owner': 'airflow',
     'start_date': datetime(2023, 10, 1),
-    'schedule' : None,
     'catchup': False
 }
 dag = DAG(
     'weather_data_orchestrator',
     default_args=dèfault_args,
-    schedule_interval=timedelta(minutes=5)
+    schedule=timedelta(minutes=5),
 )
 
 with dag:
     task1 = PythonOperator(
-        task_id='orchestrate_weather_data',
+        task_id='ingest_data_task',
         python_callable=safe_main_callable
     )
